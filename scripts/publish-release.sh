9b12b01f84058cf30b65dbeb02f7801ba813dccd
Edwin Biemond <biemond@gmail.com>
2014-10-01 16:48:14 -0700

    update

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -3,7 +3,7 @@ set -eu
 : ${DEPLOY_USERNAME:?}
 : ${DEPLOY_PASSWORD:?}
 set -x
- 
+
 VERSION=`cat artifacts/version`
 
 curl \
@@ -11,10 +11,10 @@ curl \
   --basic --user "$DEPLOY_USERNAME:$DEPLOY_PASSWORD" \
   --form r=releases \
   --form hasPom=false \
-  --form g=com.example.rest.person \
-  --form a=PersonService \
+  --form g=oracle.demo \
+  --form a=myservice \
   --form v="$VERSION" \
-  --form e=war \
-  --form p=war \
-  --form file="@artifacts/PersonService-$VERSION.war" \
+  --form e=sar \
+  --form p=sar \
+  --form file="@artifacts/sca_myservice_rev$VERSION.sar" \
   http://localhost:8081/nexus/service/local/artifact/maven/content
